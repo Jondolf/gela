ae438723936dd30b79e16a653c622903dff11998
Joona Aalto <jondolf.dev@gmail.com>
2026-09-13 03:15:52 +0300

    Improve crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,70 @@
-//! Math library for generic linear algebra and graphics programming.
+//! Generic linear algebra for games and graphics.
+//!
+//! # Types
+//!
+//! `gela` provides the following types for linear algebra:
+//!
+//! - [Vectors](vector): [`GVec2`], [`GVec3`], [`GVec4`], [`Vec3A`], [`Vec4A`], [`BVec3A`], [`BVec4A`]
+//! - [Matrices](matrix): [`GMat2`], [`GMat3`], [`GMat4`], [`Mat2A`], [`Mat3A`], [`Mat4A`]
+//! - [Rotations](rotation): [`GRot2`], [`GRot3`], [`Rot3A`]
+//! - [Isometries](isometry): [`GIso2`], [`GIso3`], [`Iso3A`]
+//! - [Affine transformations](affine): [`GAffine2`], [`GAffine3`], [`Affine2A`], [`Affine3A`]
+//!
+//! Types prefixed with `G` are generic over the element type, and types
+//! suffixed with `A` are aligned for narrow SIMD and use SIMD instructions
+//! for some operations.
+//!
+//! Additional type aliases for different element types are also provided
+//! for convenience, such as [`Vec2`], [`DRot3`], and [`Vec3x4`].
+//!
+//! # Getting Started
+//!
+//! Add `gela` to your `Cargo.toml`:
+//!
+//! ```toml
+//! [dependencies]
+//! # Replace `*` with the latest version
+//! gela = "*"
+//! ```
+//!
+//! Now, you can start writing math! A simple example is integrating the equations
+//! of motion for some game objects:
+//!
+//! ```rust
+//! use gela::vectors::Vec3;
+//!
+//! // This method is known as semi-implicit Euler integration
+//! // https://gafferongames.com/post/integration_basics/
+//! fn integrate_movement(
+//!     pos: &mut Vec3,
+//!     vel: &mut Vec3,
+//!     acc: Vec3,
+//!     dt: f32,
+//! ) {
+//!     *vel += acc * dt;
+//!     *pos += *vel * dt;
+//! }
+//! ```
 //!
 //! # Feature Flags
 //!
-//! | Feature         | Description                                                          |
-//! | --------------- | -------------------------------------------------------------------- |
-//! | `std`           | Standard library math routines instead of portable approximations    |
-//! | `wide`          | SIMD support using [`wide`] (works on stable Rust)                   |
-//! | `portable_simd` | SIMD support using [`core::simd`] (requires nightly Rust)            |
-//! | `approx`        | Approximate equality comparisons with [`approx`]                     |
-//! | `arbitrary`     | Arbitrary structured value generation with [`arbitrary`]             |
-//! | `bytecheck`     | Validation of archived types with [`bytecheck`], implies `rkyv`      |
-//! | `bytemuck`      | Casting types to and from bytes with [`bytemuck`]                    |
-//! | `cuda`          | Alignment of types matching the requirements of CUDA                 |
-//! | `encase`        | Writing types into and reading them from GPU buffers with [`encase`] |
-//! | `mint`          | Conversion to and from the interoperability types of [`mint`]        |
-//! | `rand`          | Random sampling of types with [`rand`]                               |
-//! | `rkyv`          | Zero-copy serialization and deserialization with [`rkyv`]            |
-//! | `serde`         | Serialization and deserialization with [`serde`]                     |
-//! | `speedy`        | Serialization and deserialization with [`speedy`]                    |
-//! | `zerocopy`      | Casting types to and from bytes with [`zerocopy`]                    |
+//! | Feature         | Description                                                                 |
+//! | --------------- | --------------------------------------------------------------------------- |
+//! | `std`           | Standard library math routines instead of portable approximations           |
+//! | `wide`          | Narrow SIMD support using [`wide`] (works on stable Rust)                   |
+//! | `portable_simd` | Narrow SIMD support using [`core::simd`] (requires nightly Rust)            |
+//! | `approx`        | Approximate equality comparisons with [`approx`]                            |
+//! | `arbitrary`     | Arbitrary structured value generation with [`arbitrary`]                    |
+//! | `bytecheck`     | Validation of archived types with [`bytecheck`], implies `rkyv`             |
+//! | `bytemuck`      | Casting types to and from bytes with [`bytemuck`]                           |
+//! | `cuda`          | Alignment of types matching the requirements of CUDA                        |
+//! | `encase`        | Writing types into and reading them from GPU buffers with [`encase`]        |
+//! | `mint`          | Conversion to and from the interoperability types of [`mint`]               |
+//! | `rand`          | Random sampling of types with [`rand`]                                      |
+//! | `rkyv`          | Zero-copy serialization and deserialization with [`rkyv`]                   |
+//! | `serde`         | Serialization and deserialization with [`serde`]                            |
+//! | `speedy`        | Serialization and deserialization with [`speedy`]                           |
+//! | `zerocopy`      | Casting types to and from bytes with [`zerocopy`]                           |
 //!
 //! The default features are `std` and `wide`.
 //!
@@ -70,6 +116,9 @@ pub use features::UnpaddedElement;
 pub use features::{UniformGVec2, UniformGVec3, UniformGVec4};
 #[cfg(all(feature = "rand", feature = "simd"))]
 pub use features::{UniformVec3A, UniformVec4A};
+
+#[allow(unused_imports, reason = "used in documentation")]
+use crate::prelude::*;
 
 /// Re-exports for convenience.
 pub mod prelude {
